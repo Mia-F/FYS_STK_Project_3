c54854d78130edb68b23016d254fb41f4d25ab74
Mia-F <mia.frivik@gmail.com>
2023-12-13 13:29:03 +0100

    Finishing Decision tree

--- Code/Decision_Tree.py
+++ Code/Decision_Tree.py
@@ -1,3 +1,11 @@
+"""
+This documnet contains the two classes Decision_tree and predict_future_tree
+The code in this document are based on the following codes:
+https://scikit-learn.org/stable/auto_examples/tree/plot_cost_complexity_pruning.html
+https://www.kaggle.com/code/rishidamarla/stock-market-prediction-using-decision-tree
+https://www.kaggle.com/code/prashant111/decision-tree-classifier-tutorial
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
@@ -15,6 +23,28 @@ import pandas as pd
 random.seed(10)
 class Decision_tree:
     def __init__(self,  X_train, y_train, X_test, y_test, printing=False, depth=3, randomnes = 0, alpha= False, method="squared_error") -> None:
+        """
+        Description:
+        ------------
+        A class that uses decision tree to create a model of closing prices of Bitcoins
+        Parameters:
+        ------------
+            I    X_train (pd.data_frame): A pandas dataframe containing the different parameters the model is trained on.
+            II   Y_train (pd.data_frame): A pandas dataframe containing the targets the model is trained on. (for these runs it is set to the closing time)
+            III  X_test (pd.data_frame): Same as X_train only with the test data
+            IV   Y_test (pd.data_frame): Same as y_train only with the test data
+            V    printing (Boolean): If True the class will print results while running, default False
+            VI   depth (int): The max depth of the decision tree
+            VII  randomnes (int): The random state used in DecisionTreeRegressor
+            VIII alpha (Boolean): If True the prediction function will plot dependencies on alpha (pruning) and not return y_pred and y_pred_test, default Flase
+            IX   method (str): a string of either squared_error, friedman_mse, absolute error or poisson that is used as criterion for DecisionTreeRegressor  
+        functions:
+            I    predict:  If alpha is False, the function returns y_pred and y_pred_test which is the predictions for this model.
+                           If alpha is True, the dependencis for alpha is plotted.
+            II   R2_score: A function that calculates the R2 score for the test and train data and returns these.
+            III  mse:      A function that calculates the MSE values for test and train and returns these. 
+        ------------       
+        """
         self.depth = depth
         self.X_train = X_train
         self.y_train = y_train
@@ -27,11 +57,22 @@ class Decision_tree:
     
     def predict(self):
         """
-        A function using skit-learns decisonTreeRegressor to fit the model to the dataset 
+        Description:
+        ------------
+            A function using skit-learns decisonTreeRegressor to fit the model to the training dataset 
+        Returns:
+        ------------
+            I  y_pred (np.ndarray): An array containing the model of the Bitcoin closing prices of the test data  
+            II y_pred_train (np.ndarray): An array containing the model of the Bitcoin closing prices of the train data     
+        ------------
         """
-        clf = DecisionTreeRegressor(criterion=self.method, max_depth=self.depth, random_state=self.randomnes, ccp_alpha=self.alpha)
+    
+        clf = DecisionTreeRegressor(criterion=self.method, max_depth=self.depth, random_state=self.randomnes)
         if self.alpha == True:
-            
+            """
+            The code for pruning is taken from https://scikit-learn.org/stable/auto_examples/tree/plot_cost_complexity_pruning.html
+            """
+
             #Define path for saving plots
             cwd = os.getcwd()
             
@@ -43,11 +84,10 @@ class Decision_tree:
             ccp_alphas, impurities = path_pruning.ccp_alphas, path_pruning.impurities
             clfs = []
 
-            fig, ax = plt.subplots()
+            fig, ax = plt.subplots(figsize=(12,6))
             ax.plot(ccp_alphas[:-1], impurities[:-1], marker="o", drawstyle="steps-post")
             ax.set_xlabel("effective alpha")
             ax.set_ylabel("total impurity of leaves")
-            #ax.set_title("Total Impurity vs effective alpha for training set")
             plt.savefig(path / "Impurity.png")
             plt.close()
 
@@ -69,39 +109,49 @@ class Decision_tree:
 
             node_counts = [clf.tree_.node_count for clf in clfs]
             depth = [clf.tree_.max_depth for clf in clfs]
-            fig, ax = plt.subplots(2, 1)
+            fig, ax = plt.subplots(2, 1, figsize=(12,6))
             ax[0].plot(ccp_alphas, node_counts, marker="o", drawstyle="steps-post")
             ax[0].set_xlabel("alpha")
             ax[0].set_ylabel("number of nodes")
-            #ax[0].set_title("Number of nodes vs alpha")
             ax[1].plot(ccp_alphas, depth, marker="o", drawstyle="steps-post")
             ax[1].set_xlabel("alpha")
             ax[1].set_ylabel("depth of tree")
-            #ax[1].set_title("Depth vs alpha")
             plt.savefig(path / "Alpha_depth.png")
             plt.close()
 
-            fig, ax = plt.subplots()
+            fig, ax = plt.subplots(figsize=(12,6))
             ax.plot(ccp_alphas, mse[:-1], label="Test")
             ax.plot(ccp_alphas, mse_train[:-1], label="Train")
             ax.set_xlabel("alpha")
             ax.set_ylabel("MSE")
-            #ax.set_title("MSE vs alpha")
             ax.legend()
             plt.savefig(path / "Alpha_mse.png")
             plt.close()
                     
         else:   
+            #Fit model with training data
             clf.fit(self.X_train, self.y_train)
+
+            #Predict a model with test data
             y_pred = clf.predict(self.X_test)
+            #Predict a model with training data
             y_pred_train = clf.predict(self.X_train)
             return y_pred, y_pred_train
         
-    
-
     def R2_score(self, y_pred, y_pred_train):
         """
-        A function returning the R2 score for both the testing and training data
+        Description:
+        ------------
+            A function that calculates the R2 score 
+        Parameters:
+        ------------ 
+            I   y_pred (np.ndarray): An array containing the model of the Bitcoin closing prices of the test data  
+            II  y_pred_train (np.ndarray): An array containing the model of the Bitcoin closing prices of the train data
+        Returns:
+        ------------
+            I   r2_score_test (float): The R2 score of the model based on test data
+            II  r2_score_train (float): The R2 score of the model based on training data     
+        ------------
         """
         r2_score_test = r2_score(self.y_test, y_pred)
         r2_score_train = r2_score(self.y_train, y_pred_train)
@@ -112,7 +162,18 @@ class Decision_tree:
     
     def mse(self, y_pred, y_pred_train):
         """
-        A function returning the MSE for both the testing and training data
+        Description:
+        ------------
+            A function that calculates the MSE 
+        Parameters:
+        ------------ 
+            I   y_pred (np.ndarray): An array containing the model of the Bitcoin closing prices of the test data  
+            II  y_pred_train (np.ndarray): An array containing the model of the Bitcoin closing prices of the train data
+        Returns:
+        ------------
+            I   r2_score_test (float): The MSE of the model based on test data
+            II  r2_score_train (float): The MSE of the model based on training data     
+        ------------
         """
         mse_test = mean_squared_error(self.y_test, y_pred)
         mse_train = mean_squared_error(self.y_train, y_pred_train)
@@ -121,56 +182,82 @@ class Decision_tree:
             print(f"Training-set MSE value: {mse_train:.4f}")
         return mse_test, mse_train
     
+
+
 class predict_future_tree:
-    def __init__(self, X,y , printing=False, data_frame=None, depth=3, predicted_days = 10, randomnes = 0, method="squared_error") -> None:
+    def __init__(self , data_frame=None, depth=3, predicted_days = 10, randomnes = 0, method="squared_error") -> None:
+        """
+        Description:
+        ------------
+        A class that uses decision tree to predict the closing price of Bitcoins
+        Parameters:
+        ------------
+            I   data_frame (pd.data_frame): A pandas data frame containing the parameters used to make a model
+            II  depth (int): The max depth of the decision tree
+            III predicted_days (int): The amount of days the price is predicted 
+            IV  randomnes (int): The random state used in DecisionTreeRegressor
+            V   method (str): a string of either squared_error, friedman_mse, absolute error or poisson that is used as criterion for DecisionTreeRegressor  
+        functions:
+            I    predict:  A function that uses decision tree to predict the closing price of Bitcoins
+        ------------       
+        """
         self.depth = depth
         self.predicted_days = predicted_days
-        self.X = X
-        self.y = y
-        self.printing = printing
         self.data_frame = data_frame
         self.randomnes = randomnes
         self.method = method
 
     def predict(self):
+        """
+        Description:
+        ------------
+            A function that Predict Bitcoin prices and plots the result to the actual price. 
+            This function is based on the following code: https://www.kaggle.com/code/rishidamarla/stock-market-prediction-using-decision-tree
+        """
 
         df = pd.DataFrame()
         df = self.data_frame 
+        #drop the label colum in datset
         df = df.drop(["Label"], axis=1)
+        #Include a prediction colum that is the price of Bitcoin when market close for all days expet the prediction days
         df["Prediction"] = df["Close"].shift(-self.predicted_days)
-
+        #removes the prediction for the prediction days from data set
         X = df.drop(["Prediction"], axis=1)[:-self.predicted_days]
-        print(X)
         y = df["Prediction"][:-self.predicted_days]
 
-       
+        #uses the datset to creat a model 
         clf = DecisionTreeRegressor(criterion=self.method, random_state=self.randomnes)
+        #Fit model with data tath does not include the predicted days
         clf.fit(X, y)
+
+        #creat data_frem with predicted days
         x_future = df.drop(["Prediction"], axis = 1)[-self.predicted_days:]
-        print(x_future)
         x_future = x_future.tail(self.predicted_days)
         
+        # Predict prices of future days
         tree_prediction = clf.predict(x_future)
         predictions = tree_prediction 
-        #print(predictions)
         
+        #Adds the result for predicted day in a separate data_frame
         valid = x_future
         valid["Prediction"] = predictions
-        print(valid.tail())
-        #print(valid.tail())
-        #df["Prediction"] = predictions
+
         cwd = os.getcwd()
         path = Path(cwd) / "FigurePlots" / "Decision_tree"
         if not path.exists():
             path.mkdir()
-        plt.figure(figsize=(16,8))
-        plt.title("Model")
-        plt.xlabel('Days')
-        plt.plot(X.loc[:,"Close"])
-        plt.plot(valid.loc[:,"Prediction"])
-        #plt.plot(self.data_frame.loc[:,"Close"])
-        plt.legend(["Original", "Predicted"])
+
+        #Plott result against actuall predicted prices
+        lenght = len(df.loc[:,"Close"])
+        days = np.linspace(lenght + 1, lenght + 101, 100)
+
+        plt.figure(figsize=(12,8))
+        plt.xlabel("Days")
+        plt.plot(self.data_frame.loc[:,"Close"], "--", label="Actual closing price")
+        plt.scatter(days, valid.loc[:,"Prediction"], label="Predicted closing price", color="tab:orange")
+        plt.legend()
         plt.savefig(path / "predicition.png")
+        plt.show()
         plt.close()
 
 
--- Code/main.py
+++ Code/main.py
@@ -18,11 +18,11 @@ params = {
     "font.family": "Serif",
     "font.serif": "Roman", 
     "text.usetex": True,
-    "axes.titlesize": fontsize,
-    "axes.labelsize": fontsize,
-    "xtick.labelsize": "x-large",
-    "ytick.labelsize": "x-large",
-    "legend.fontsize": fontsize
+    "axes.titlesize": "large",
+    "axes.labelsize": "large",
+    "xtick.labelsize": "large",
+    "ytick.labelsize": "large",
+    "legend.fontsize": "large"
 }
 
 plt.rcParams.update(params)
@@ -85,7 +85,7 @@ def testing_decision_tress(method, n_min, n_max, path):
                 lowest_mse_index = [depth[i],randomnes[j]]
     
     #Plot result and save figurs for MSE and R2 score
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(12,6))
     m = ax.imshow(mse,extent=[0,10,n_max,n_min])
     ax.set_aspect('equal')
     fig.colorbar(m, label="MSE")
@@ -94,7 +94,7 @@ def testing_decision_tress(method, n_min, n_max, path):
     plt.savefig(path / "mse.png")
     plt.close()
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(12,6))
     m = ax.imshow(mse_train_2D, extent=[0,10,n_max,n_min])
     fig.colorbar(m, label="MSE")
     ax.set_xlabel("Random state")
@@ -102,21 +102,21 @@ def testing_decision_tress(method, n_min, n_max, path):
     plt.savefig(path / "mse_train.png")
     plt.close()
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(12,6))
     ax.plot(depth, mse[:,lowest_mse_index[1]])
     ax.set_xlabel("Depth")
     ax.set_ylabel("MSE")
     plt.savefig(path / "mse_depth.png")
     plt.close()
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(12,6))
     ax.plot(depth, r2[:,lowest_mse_index[1]])
     ax.set_xlabel("Depth")
     ax.set_ylabel("R2 score")
     plt.savefig(path / "r2_depth.png")
     plt.close()
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(12,6))
     ax.plot(depth, r2_train_2D[:,lowest_mse_index[1]])
     ax.set_xlabel("Depth")
     ax.set_ylabel("R2 score")
@@ -132,16 +132,19 @@ data_frame.add_technical_indicators()
 ta_data = data_frame.extract_data_for_NN()
 
 #Creat define matrix and validation array
-X = ta_data.drop(["Target"], axis=1)
-y = ta_data.loc[:, "Target"]
+X = ta_data.drop(["Close", "Target"], axis=1)
+y = ta_data.loc[:, "Close"]
 
 #Define min and max depth for tree
 n_max = 10
-n_min = 4
+n_min = 1
 
 #split in to train test 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.8)
-scaler = MinMaxScaler(feature_range=(0,1))
+
+#Scaleing the data
+#scaler = MinMaxScaler(feature_range=(0,1))
+scaler = StandardScaler()
 scaler.fit(X_train)
     
 X_train_scaled = scaler.transform(X_train)
@@ -150,45 +153,69 @@ X_test_scaled = scaler.transform(X_test)
 y_train_scaled = (y_train - np.mean(y_train))/np.std(y_train)
 y_test_scaled = (y_test - np.mean(y_train))/np.std(y_train)
 
+#Creating an array with the criterions for DecisionTreeRegressor
 methods = np.array(["squared_error", "friedman_mse","absolute_error"])
 
 lowest_mse= []
 mse = []
 
 i=0
+
+#Runs through the 3 diffrent criterions and 10 diffrent random states
 for method in methods:
-    print(i)
     print(f"method = {method}")
+
+    #Creat path for figurs
     cwd = os.getcwd()
     path = Path(cwd) / "FigurePlots" / "Decision_tree"/ method
     if not path.exists():
         path.mkdir()
     
+    #Get the mse for the diffrent method and at witch depth and random state the lowest mse value is uptainded for each method
     lowest_mse_method, mse_method = testing_decision_tress(method,n_min, n_max, path)
+    #Se how it is affected by pruning the tree
     Model = Decision_tree(X_train_scaled, y_train_scaled, X_test_scaled, y_test_scaled, printing=True, depth=lowest_mse_method[0], randomnes=lowest_mse_method[1], alpha=True, method = method )
     Model.predict()
+
     lowest_mse.append(lowest_mse_method)
     index = lowest_mse_method[1]
     index = int(index)
     mse.append(mse_method[:,index-1])
     i += 1
 
+#change list to array
 mse = np.array(mse)
+lowest_mse = np.array(lowest_mse)
+
+print("\n")
+print("----------------------------------- Results -----------------------------------")
+print(f"{methods[0]} had a lowest mse of {np.min(mse[0,:]):.5f}, when depth = {lowest_mse[0][0]} and random stat = {lowest_mse[0,1]}")
+print(f"{methods[1]} had a lowest mse of {np.min(mse[1,:]):.5f}, when depth = {lowest_mse[1][0]} and random stat = {lowest_mse[1,1]}")
+print(f"{methods[2]} had a lowest mse of {np.min(mse[2,:]):.5f}, when depth = {lowest_mse[2][0]} and random stat = {lowest_mse[2,1]}")
+print("-------------------------------------------------------------------------------")
 
 path = Path(cwd) / "FigurePlots" / "Decision_tree"
 if not path.exists():
     path.mkdir()
 
+
 depth = np.linspace(n_min, n_max, n_max-n_min).astype(int)
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(12,6))
 ax.plot(depth, mse[0,:], label="Square error")
 ax.plot(depth, mse[1,:], label="Friedman mse")
 ax.plot(depth, mse[2,:], label="Absolute error")
 ax.legend()
+ax.set_xlabel("Depth")
+ax.set_ylabel("MSE")
 plt.savefig(path / "mse_different_methods.png")
 plt.close()
 
+#Find which method that had the lowest mse and uptain an index to use for prediction
+index = np.where(mse == np.min(mse))
+method = methods[index[0]][0]
+random_state = lowest_mse[index[0],1][0]
 
-prediciton_model = predict_future_tree(X, y , printing=False, data_frame=ta_data, depth=4, predicted_days = 100, randomnes = 20, method = "friedman_mse")
+#Use the best model found to predict Bitcoin prices
+prediciton_model = predict_future_tree(data_frame=ta_data, depth=lowest_mse[index[0],0], predicted_days = 100, randomnes = random_state , method = method)
 prediciton_model.predict()
 
