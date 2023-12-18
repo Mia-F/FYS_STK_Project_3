dee408325e744a3b071ffd0cb43d9a5143b443d2
Mia-F <mia.frivik@gmail.com>
2023-12-18 14:41:23 +0100

    fixing plot

--- Code/NN_BTC_PROJ.py
+++ Code/NN_BTC_PROJ.py
@@ -272,7 +272,7 @@ if __name__ == "__main__":
 
             prev_y_test = y_test_r2[-window_size:]
             prev_predictions = predictions_r2[-window_size:]
-  
+ 
     plt.plot(days, daily_mse_values_test)
     plt.xlabel("Days since november 15, 2014")
     plt.ylabel("MSE")
@@ -305,14 +305,14 @@ if __name__ == "__main__":
     total_inaccurate_predictions = sum(inaccurate_count_values)
     average_accuracy = total_accurate_predictions / (total_accurate_predictions + total_inaccurate_predictions)
 
-
+ 
     print(f'Average MSE: {average_mse}')
     print(f'Average MAE: {average_mae}')
     print(f'Average R2: {average_r2}')
     print(f'Average MAPE: {average_mape}')
     print(f'Average MAPE: {average_max_error}')
     print(f'Average accuarcy:{average_accuracy}')
-
+ 
     # Setting days for plot
     days = 100 #* 30
     last_100_predictions = final_predictions[-days:]
@@ -353,10 +353,11 @@ if __name__ == "__main__":
     # Save the plot
     plt.savefig(f'last_100_lstm.png', bbox_inches='tight')
     plt.show()
+
   
 
     # Setting days for plot
-    days = 100 #* 30
+    days = 365 #* 30
     last_100_predictions = final_predictions[-days:]
     last_100_actuals = final_actuals[-days:]
 
@@ -372,7 +373,6 @@ if __name__ == "__main__":
             cross_points_y.append(last_100_actuals[i])
 
     # Plotting
-
     plt.figure(figsize=(9, 5))
     plt.plot(last_100_actuals, label='Actual Prices', color='tab:blue', linestyle='-', linewidth=2)
     plt.plot(last_100_predictions, label='Predicted Prices', color='tab:red', linestyle='-.', linewidth=2)
@@ -396,14 +396,21 @@ if __name__ == "__main__":
     # Save the plot
     plt.savefig(f'last_365_lstm.png', bbox_inches='tight')
     plt.show()
-  
+
+    #Setting path for figure
+    cwd = os.getcwd()
+    path = Path(cwd) / "FigurePlots" / "LSTM" / "predicted_vs_actuals"
+
+    if not path.exists():
+        path.mkdir()
 
     #Plot the difference bewteen actual and predicted prices
     last_100_actuals = np.array(last_100_actuals)
     last_100_predictions = np.array(last_100_predictions)
-    print(last_100_actuals)
-    print(last_100_predictions)
-    plt.plot(last_100_actuals - last_100_predictions)
-    plt.xlabel(f'Time (Last {days} Days)', fontsize=14)
-    plt.ylabel("Difference between actual and predicted price", fontsize=14)
+
+    fig, ax = plt.subplots(figsize=(9, 5))
+    ax.plot(last_100_actuals - last_100_predictions)
+    ax.set_xlabel(f'Time (Last {days} Days)')
+    ax.set_ylabel("Difference between actual and predicted price")
+    plt.savefig(path / f"Difference_between_actual_and_predicted_365_NN.png")
     plt.show()
